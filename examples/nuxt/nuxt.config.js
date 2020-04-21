d52f4513e09ca0d51df3b262dee7e7af8219518c
NikulinIlya <ilia.nikulin@gmail.com>
2020-04-21 10:49:11 +0300

    fix(examples): update nuxt example

--- examples/nuxt/nuxt.config.js
+++ examples/nuxt/nuxt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // some nuxt config...
   plugins: [
-    { src: '~plugins/nuxt-easymde-plugin.js', ssr: false },
+    { src: '~plugins/nuxt-easymde-plugin.js', mode: 'client' },
   ],
   css: [
     'easymde/dist/easymde.min.css',
