7b491cf7cbcf658c671207d3bb12ace2047e9844
NikulinIlya <ilia.nikulin@gmail.com>
2020-04-21 10:47:31 +0300

    fix(examples): update nuxt example

--- examples/index.vue
+++ examples/index.vue
@@ -1,81 +1,81 @@
 <template>
   <div>
     <!-- use v-model control value -->
-    <vue-easyemde v-model="content" ref="markdownEditor" />
+    <vue-easyemde v-model="content" ref="markdownEditor"/>
 
     <!-- use event control value -->
-    <vue-easyemde :value="content" @input="handleInput" />
+    <vue-easyemde :value="content" @input="handleInput"/>
 
     <!-- add config -->
-    <vue-easyemde :configs="configs" />
+    <vue-easyemde :configs="configs"/>
 
     <!-- disable auto init -->
-    <vue-easyemde :autoinit="false" />
+    <vue-easyemde :autoinit="false"/>
   </div>
 </template>
 
 <script>
-  import VueEasymde from 'vue-easymde/src/vue-easyemde'
+  import VueEasymde from 'vue-easymde';
 
   // Base example
   export default {
     components: {
-        VueEasymde
+      VueEasymde,
     },
-    data () {
+    data() {
       return {
         content: '',
         configs: {
           spellChecker: false // disable spell check
-        }
-      }
-    }
-  }
+        },
+      };
+    },
+  };
 
   // Complete example
   export default {
     components: {
-        VueEasymde
+      VueEasymde,
     },
-    data () {
+    data() {
       return {
         content: '',
         configs: {
           status: false, // disable the status bar at the bottom
           spellChecker: false // disable spell check
-        }
-      }
+        },
+      };
     },
     computed: {
-      easymde () {
-        return this.$refs.markdownEditor.easymde
-      }
+      easymde() {
+        return this.$refs.markdownEditor.easymde;
+      },
     },
-    mounted () {
-      console.log(this.easymde)
-      this.easymde.togglePreview()
+    mounted() {
+      console.log(this.easymde);
+      this.easymde.togglePreview();
 
       // 'change' event has bound, via @input attache an event listener
       // You can attache events in this [list](https://codemirror.net/doc/manual.html#events) yourself if necessary
       this.easymde.codemirror.on('beforeChange', (instance, changeObj) => {
         // do some things
-      })
+      });
 
       // remove EasyMDE, when component destroy will invoke
-      this.easymde = null
+      this.easymde = null;
 
       // some useful methods
-      this.$refs.markdownEditor.initialize() // init
-      this.easymde.toTextArea()
-      this.easymde.isPreviewActive() // returns boolean
-      this.easymde.isSideBySideActive() // returns boolean
-      this.easymde.isFullscreenActive() // returns boolean
-      this.easymde.clearAutosavedValue() // no returned value
-      this.easymde.markdown(this.content) // returns parsed html
-      this.easymde.codemirror.refresh() // refresh codemirror
+      this.$refs.markdownEditor.initialize(); // init
+      this.easymde.toTextArea();
+      this.easymde.isPreviewActive(); // returns boolean
+      this.easymde.isSideBySideActive(); // returns boolean
+      this.easymde.isFullscreenActive(); // returns boolean
+      this.easymde.clearAutosavedValue(); // no returned value
+      this.easymde.markdown(this.content); // returns parsed html
+      this.easymde.codemirror.refresh(); // refresh codemirror
     },
     methods: {
-      handleInput () {
+      handleInput() {
         // do some things
       }
     }
--- examples/nuxt/nuxt-simplemde-plugin.js
+++ examples/nuxt/nuxt-simplemde-plugin.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
 import VueEasymde from 'vue-easymde';
+import 'easymde/dist/easymde.min.css';
 
-Vue.use(VueEasymde);
+Vue.component('vue-easymde', VueEasymde);
