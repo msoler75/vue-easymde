0708209a7c07b8c8513893beffc1573779e81c6a
NikulinIlya <ilia.nikulin@gmail.com>
2020-10-01 18:51:26 +0300

    fix: update for linter

--- hack.js
+++ hack.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const html = fs.readFileSync('dist/demo.html').toString();
 
 const hackedHtml = html.replace('<script src="https://unpkg.com/vue"></script>', `
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/easymde@2.7.0/dist/easymde.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/easymde@2.12.0/dist/easymde.min.css">
 <script src="https://unpkg.com/vue"></script>
 <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/easymde@2.7.0/src/js/easymde.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/easymde@2.12.0/src/js/easymde.min.js"></script>
 `);
 
 fs.writeFileSync('dist/demo.html', hackedHtml);
